e87d7ec085ef98133b841c19b4d472ec077f7d92
ToryteX <120201332+ToryteX@users.noreply.github.com>
2023-01-17 16:57:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ToryteX/QuckToc /EvaMaria
+  git clone https://github.com/ToryteX/QuckToc /QuckToc
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /QuckToc
